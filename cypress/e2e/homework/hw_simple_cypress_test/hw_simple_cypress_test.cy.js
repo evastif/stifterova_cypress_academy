546c0b2a2e5e55018cfe4c1359069139887c2081
Eva Stifterova <estifterova@estifter-gp-1>
2025-03-14 10:13:39 +0100

    little correction

--- cypress/e2e/homework/hw_simple_cypress_test/hw_simple_cypress_test.cy.js
+++ cypress/e2e/homework/hw_simple_cypress_test/hw_simple_cypress_test.cy.js
@@ -4,7 +4,7 @@ Klikne na tlačítko Submit
 Hotovou práci napushuj do Github a vytvoř Pull Request na Petra*/
 
 describe("Simple registration test", () => {
-  const Name = "Petr Novák";
+  const name = "Petr Novák";
   const email = "petr.novak@tredgate.com";
   const password = "heslo";
   const confirmPassword = "heslo";
@@ -12,7 +12,7 @@ describe("Simple registration test", () => {
 
   it("Registration", () => {
     cy.visit("https://tredgate.com/webtrain/registration.html");
-    cy.get("#name").type(Name);
+    cy.get("#name").type(name);
     cy.get("#email").type(email);
     cy.get("#password").type(password);
     cy.get("#confirm-password").type(confirmPassword);
