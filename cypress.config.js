d80db202b13679e6dde0a48836b987e35240bb15
Eva Stifterova <estifterova@estifter-gp-1>
2025-03-26 16:49:23 +0100

    cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  env: {
+    pmtool_url: "https://tredgate.com/pmtool",
+    eshop_url: "https://tredgate.com/eshop",
+    webtrain_url: "https://tredgate.com/webtrain",
+    automation_test_store_url: "https://automationteststore.com/",
+    tegb_url: "http://localhost:3001",
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     watchForFileChanges: false,
-    chromeWebSecurity: false,
     baseUrl: "https://tredgate.com/pmtool",
   },
 });
